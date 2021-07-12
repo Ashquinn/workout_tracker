a62633fbaefb3fb8901093fa3698decb6d61df9d
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-07-12 18:50:26 -0400

    corrected the casing of the database, required htmlRoutes and apiRoutes

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(logger("dev"));
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
-
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect('mongodb://localhost/Workout', {
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   });
 
-  app.use(require("./routes/api.js"));
+  require('./routes/htmlRoutes.js')(app);
+  app.use(require('./routes/apiRoutes.js'));
 
   app.listen(PORT, () => {
       console.log(`App running on port ${PORT}`)
